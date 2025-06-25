da7d4c7d2e3521f99366efe7e8a0c8266adc8160
Enimoop <marine.forcioli@laposte.net>
2025-06-25 17:13:47 +0200

    removed empty cells, only cells with pokemon appears

---
+++ pokedex/front_end/pokedex_front_app/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import './pc.css';
+
+export default function Navbar() {
+  return (
+    <div className="box-nav">
+      <div className="arrow">{'◀'}</div>
+      <div className="box-title">Box 1</div>
+      <div className="arrow">{'▶'}</div>
+    </div>
+  );
+}
